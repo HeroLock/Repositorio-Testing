f9f5b1fe1954add6c8335fe72a1c20e8e7803d1b
Engell <engellvado08@gmail.com>
2024-10-09 23:25:59 -0600

    modificaciones en ambos documentos

--- testeo.js
+++ testeo.js
@@ -1,79 +1,113 @@
 // PRUEBA DE ESTRUCTURAS EN JAVASCRIPT
 
-//funciones, for, if
-// function comprobarEstudiantes(){
+// funciones, for, if
+function comprobarEstudiantes(){
 
-// let estudiantes = ['Juan', 'Luis', 'Carlos', 'Engell', 'Gabriel', 'Alejandro', 'Edward', 'Joseph']
-// let notas = [85, 67, 90, 45, 78, 82, 59, 100]
-// //contadores
-// let qaprobados = 0
-// let qreprobados = 0
-// for(let i = 0; i < notas.length; i++){
-//     if(notas[i] >= 70){
-//         //console.log(notas[0])
-//         qaprobados++
-//         console.log("El estudiante: " + estudiantes[i] + " esta aprobado")
-//         console.log("Su nota es : " + notas[i])
-//     }else{
-//         //console.log(lol)
-//         qreprobados++
-//         console.log("El estudiante: " + estudiantes[i] + " esta reprobado")
-//         console.log("Su nota es : " + notas[i])
-//     }
-// }
-//     console.log("La cantidad de estudiantes aprobados es: " + qaprobados)
-//     console.log("La cantidad de estudiantes reprobados es: " + qreprobados)
-// }
+let estudiantes = ['Juan', 'Luis', 'Carlos', 'Engell', 'Gabriel', 'Alejandro', 'Edward', 'Joseph']
+let notas = [85, 67, 90, 45, 78, 82, 59, 100]
+//contadores
+let qaprobados = 0
+let qreprobados = 0
+for(let i = 0; i < notas.length; i++){
+    if(notas[i] >= 70){
+        //console.log(notas[0])
+        qaprobados++
+        console.log("El estudiante: " + estudiantes[i] + " esta aprobado")
+        console.log("Su nota es : " + notas[i])
+    }else{
+        //console.log(lol)
+        qreprobados++
+        console.log("El estudiante: " + estudiantes[i] + " esta reprobado")
+        console.log("Su nota es : " + notas[i])
+    }
+}
+    console.log("La cantidad de estudiantes aprobados es: " + qaprobados)
+    console.log("La cantidad de estudiantes reprobados es: " + qreprobados)
+}
 
 // comprobarEstudiantes();
 
 // ----------------------------------------------------------------------------------------------------------------------------------------------------------
 
-//objetos
-// let Persona = {
-//     nombre : "Engell",
-//     edad : 18,
-//     masculino: true,
-// }
-// console.log(Persona)
-// Persona.musicaFavorita = "Rap" // Crear atributos de un objeto fuera de el
+// Objetos
+let trabajador = {
+    nombre : "Engell",
+    edad : 18,
+    masculino: true,
+}
+//console.log(Persona)
+trabajador.musicaFavorita = "Rap" // Crear atributos de un objeto fuera de el
 
-// let Persona2 = {
-//     nombre : "Gabriela",
-//     edad : 17,
-//     masculino: false,
-// }
-// console.log(Persona2)
-// Persona2.edad = 22 //Modificar un atributo ya existente
+let trabajador2 = {
+    nombre : "Gabriela",
+    edad : 17,
+    masculino: false,
+}
+//console.log(Persona2)
+trabajador2.edad = 22 //Modificar un atributo ya existente
 
-// let arregloDePersonas = [Persona, Persona2]
+let arregloDeTrabajadores = [trabajador, trabajador2]
 
 // ----------------------------------------------------------------------------------------------------------------------------------------------------------
+// Trabajo Propio
+
+let txtPrPe = document.getElementById("prPe")
+let btnCheck = document.getElementById("chequeo")
+let pStatusAgN = document.getElementById("estadoAgregarNegativo")
+let txtPrNm = document.getElementById("prNm")
+let txtPrEd = document.getElementById("prEd")
+let txtPrPs = document.getElementById("prPs")
+let pStatusAgP = document.getElementById("estadoAgregarPositivo")
+
+
 let personas = []
+let estadoAgregarPositivo 
+let estadoAgregarNegativo 
 
 function agregarPersonas(){
+    pStatusAgN.innerText = estadoAgregarNegativo
 
 personas.push({
-    nombre : prompt("¿Cuál es su nombre?"),
-    edad : prompt("¿Cuál es su edad?"),
-    peso : prompt("¿Cuál es su peso?"),
+    // nombre : prompt("¿Cuál es su nombre?"),
+    // edad : prompt("¿Cuál es su edad?"),
+    // peso : prompt("¿Cuál es su peso?"),
+    nombre : txtPrNm,
+    edad : txtPrEd,
+    peso : txtPrPs,
 })
-    alert("Agregaste a esta persona");
+    //alert(estadoAgregar);
+    pStatusAgP.innerText = estadoAgregarPositivo
 }
 
+    // let preguntarPersonas = parseInt(prompt("Cuantas personas quieres agregar?", 10))
+    let preguntarPersonas
 
-    let preguntarPersonas = parseInt(prompt("Cuantas personas quieres agregar?"))
-    
+    function checkPreguntar(){
 
-    if(!isNaN(preguntarPersonas && preguntarPersonas > 0)){
+        preguntarPersonas = parseInt(txtPrPe.value, 10)
+
+    if(!isNaN(preguntarPersonas) && preguntarPersonas > 0){
         for(let i = 0; i < preguntarPersonas; i++){
+            estadoAgregarNegativo = "OK"
+            estadoAgregarPositivo = "Se va a agregar esta persona"
             agregarPersonas()
         }
-    }else if(preguntarPersonas == 0){
-        alert("No agregaste personas")
+    }else if(preguntarPersonas === 0){
+        estadoAgregarNegativo = "No agregaste personas"
+        estadoAgregarPositivo = null
+        //alert(estadoAgregar);
+        pStatusAgN.innerText = estadoAgregarNegativo
+        pStatusAgP.innerText = estadoAgregarPositivo
     }else{
-        alert("No dijiste cuantas personas querias agregar!")
+        estadoAgregar = "No dijiste cuantas personas querias agregar!"
+        estadoAgregarPositivo = null
+        //alert(estadoAgregar);
+        pStatusAgN.innerText = estadoAgregarNegativo
+        pStatusAgP.innerText = estadoAgregarPositivo
     }
+    }
+    btnCheck.addEventListener("click", checkPreguntar);
+    checkPreguntar()
 
     let numPersonas = personas.length
 
